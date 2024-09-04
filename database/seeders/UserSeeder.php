0bb60b250fdbc32390e51e9beb2bfa24e95cfd86
= <gadieldjibom961@gmail.com>
2024-09-04 08:51:33 +0000

    initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        User::factory()->create([
+            'name' => 'ariel',
+            'email' => 'clemence@.com',
+            'password' => '123456'
+        ]);
+    }
+}
